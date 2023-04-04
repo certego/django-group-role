3e4da3da4e0dd1fdd9015f6ceb390bf58ad37362
Davide <d.setti@certego.net>
2023-04-04 17:41:14 +0200

    Version bump

--- django_group_role/__init__.py
+++ django_group_role/__init__.py
@@ -2,4 +2,4 @@ from .exceptions import BadRoleException
 from .roles import Role, load_roles, registry
 from .signals import post_role_setup, pre_role_setup
 
-__version__ = (0, 5, 0)
+__version__ = (0, 5, 1)
