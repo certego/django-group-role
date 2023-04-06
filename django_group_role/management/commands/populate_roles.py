1be23e50bc469b9446dbf1a1922b41c7ca91f500
Davide <d.setti@certego.net>
2023-04-06 09:05:41 +0200

    Remove unused imports in command

--- django_group_role/management/commands/populate_roles.py
+++ django_group_role/management/commands/populate_roles.py
@@ -1,6 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import Permission
-from django.conf import settings
 from ...roles import registry, load_roles, BadRoleException
 
 
